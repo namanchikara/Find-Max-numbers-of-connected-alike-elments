5da7a69170242575f9a9fe25452d915b0ad89da2
Naman Chikara <34776247+namanchikara@users.noreply.github.com>
2018-08-24 04:48:33 -0700

    Create FindMax.py

---
+++ FindMax.py
@@ -0,0 +1,82 @@
+class FindMax:
+  def array(self,array):
+    self.visited = []
+    self.tovisit = [[0,0]]
+    self.array = array
+
+  def MaxForCurrentCoordinates(self, x,y):
+      count = 0
+      if [x,y] not in self.visited:
+          self.visited.append([x,y])
+          
+          try:
+              if self.array[x][y] == self.array[x+1][y]:
+                  count = 1 + self.MaxForCurrentCoordinates(x+1, y)
+                  
+              else:
+                  count = 1
+                  if [x+1,y] not in self.tovisit and [x+1,y] not in self.visited:
+                      self.tovisit.append([x+1,y])
+          except:
+              count += 1
+              
+          try:
+                  
+              if self.array[x][y] == self.array[x][y+1]:
+                  count += self.MaxForCurrentCoordinates(x, y+1)
+              
+              else:
+                  if [x,y+1] not in self.tovisit and [x,y+1] not in self.visited:
+                      self.tovisit.append([x,y+1])
+                  
+          except:
+              pass
+          
+          try:
+              if (x-1)>=0:
+                  
+                  if self.array[x][y] == self.array[x-1][y]:
+                      count += self.MaxForCurrentCoordinates(x-1,y)
+                  
+                  else:
+                      if [x-1,y] not in self.tovisit and [x-1,y] not in self.visited:
+                          self.tovisit.append([x-1,y])
+          except:
+              pass
+          
+          try:
+              if (y-1)>=0:
+                  if self.array[x][y-1] == self.array[x][y]:
+                      count += self.MaxForCurrentCoordinates(x,y-1)
+                  
+                  elif x!=0 and y-1!=0:
+                      self.tovisit.append([x][y-1])
+          
+          except:
+              pass
+          
+      return count
+
+
+  def FindMaxNow(self):
+      mcount = 0
+      while self.tovisit:
+          x , y = self.tovisit.pop()
+          if [x,y] not in self.visited:
+              count = self.MaxForCurrentCoordinates(x,y)
+              if mcount < count:
+                  mcount = count
+      
+      return(mcount)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    array = list()
+    for i in range(n):
+        array.append(list(map(int, input().split())))
+
+
+    object = FindMax()
+    object.array(array)
+    print(object.FindMaxNow())
